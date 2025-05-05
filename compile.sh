d7443fcfd2be5d5dd4ef868e286c65c4e550de4a
Your Full Name <your@email.tld>
2025-05-05 14:11:52 +0200

    changed compilation option

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 cmake -B build
-cmake --build build
+cmake --build build --config Release -j $(nproc)
 go get github.com/newrelic/go-agent/v3/newrelic
 go get github.com/newrelic/go-agent/v3/integrations/nrgin
 go build .
